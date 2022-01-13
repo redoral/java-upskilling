6119841ea83e43efddf6a94f2ab670ef3a12bd28
unknown <redmunozoral@gmail.com>
2022-01-13 09:12:16 -0800

    Typo fix

--- week2/day4/RunnableHandsOn/src/com/revature/app/Main.java
+++ week2/day4/RunnableHandsOn/src/com/revature/app/Main.java
@@ -8,13 +8,13 @@ public class Main {
         // Defines the abstract methods from the functional interface
         CalculatorFunction calculatorFunctionAdd = (a,b)-> a+b;
         CalculatorFunction calculatorFunctionSubtract = (a,b)-> a-b;
-        CalculatorFunction calculatorFunctionMultliply = (a,b)-> a*b;
+        CalculatorFunction calculatorFunctionMultiply = (a,b)-> a*b;
         CalculatorFunction calculatorFunctionDivide = (a,b)-> a/b;
 
         // Calls the methods
         int add = calculatorFunctionAdd.calculate(2,2);
         int sub = calculatorFunctionSubtract.calculate(5,3);
-        int mul = calculatorFunctionMultliply.calculate(10,10);
+        int mul = calculatorFunctionMultiply.calculate(10,10);
         int div = calculatorFunctionDivide.calculate(10,2);
 
         // Prints out the results
